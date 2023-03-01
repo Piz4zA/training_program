d48c3ca274e0caa54f405cdc1e32ce0312062085
Isanin Sergey <isanin.pro@gmail.com>
2023-03-01 23:43:09 +0300

    uppdate. video recorded

--- javascript/2.2/digits.js
+++ javascript/2.2/digits.js
@@ -1,11 +1,11 @@
 let digit = -25;
-let tract = -25.03;
-let exp = -2e10; // 20000000000
+let fract = -25.02;
+let exp = -2e10; // 20 000 000 000
 let expSmall = -2e-3; // 0.002
 let expFract = -1.25e3; // 1250
-let expFractSmall = -1.25e-3; // 0.00125
-let fract1 = -25;
-let fract2 = -0.25;
+let expFractSmall = -1.25e-3; // 0.0125
+let fract1 = -25.;
+let fract2 = -.25;
 
 // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 // 0, 1, 10, 11, 100, 101, 110, 111, 1000, 1001
@@ -20,7 +20,7 @@ let oct = 0o10;
 // 16
 
 let x = 0x1b; // 27
-let X = 0x1b; // 27
+let X = 0x1B; // 27
 
-// rgb(255, 255, 255)
-// #fffffff
+// rgb (255, 255, 255)
+// #ffffff
